0013818f73eb356895e2cacbb54aaf1d99bc7e63
Viet Nguyen <vietviet2002@gmail.com>
2022-03-27 21:10:28 +0700

    Add menu v1

---
+++ js/menu.js
@@ -0,0 +1,139 @@
+
+const body = document.querySelector("body");
+const menu_items = document.querySelectorAll(".menu__container__item");
+
+const menu_right_btn_open = document.querySelector(".menu--right__button");
+const menu_right_btn_close = document.querySelector(".main-menu__btn");
+const menu_right = document.querySelector(".main-menu");
+
+const menu_bottom = document.querySelector(".menu--bottom");
+const menu_bottom__close = document.querySelector(".menu--bottom__close");
+const menu_bottom__open = document.querySelector(".menu--bottom__open");
+
+const map_btn = document.querySelector(".map");
+const map_iframe = document.querySelector(".map-iframe");
+const map_close = document.querySelector(".map-close");
+
+const info_btn = document.querySelector(".info");
+const info_iframe = document.querySelector(".info-iframe");
+const info_close = document.querySelector(".info-iframe-close");
+
+const fullscreen_btn = document.querySelector(".fullscreen");
+const fullscreen_desc = document.querySelector(".fullscreen-desc");
+
+const video_player = document.querySelector(".video-player");
+const main_video = document.querySelector(".main-video");
+const video_player_background = document.querySelector(".video-player-background");
+const video_1 = document.querySelector(".video-1");
+const video_2 = document.querySelector(".video-2");
+const link_video1 = "https://www.youtube.com/embed/R9At2ICm4LQ"; // Change ID to change video
+const link_video2 = "https://www.youtube.com/embed/tgbNymZ7vqY";
+
+const main_audio = document.querySelector(".main-audio");
+const sound = document.querySelector(".sound");
+const sound_desc = document.querySelector(".sound-desc");
+
+// Hiện submenu menu bottom
+menu_items.forEach(item => {
+  item.addEventListener("click", () => {
+    item.querySelector(".sub-menu").classList.toggle("active");
+  })
+});
+
+// Đóng mở menu bottom
+menu_bottom__close.addEventListener("click", () => {
+  menu_bottom.classList.remove("active");
+  menu_bottom__open.classList.add("active");
+});
+menu_bottom__open.addEventListener("click", () => {
+  menu_bottom.classList.add("active");
+  menu_bottom__open.classList.remove("active");
+})
+
+// Đóng mở menu phải
+menu_right_btn_open.addEventListener("click", () => {
+  menu_right.classList.add("active");
+  menu_right_btn_open.style.display = "none";
+});
+menu_right_btn_close.addEventListener("click", () => {
+  menu_right.classList.remove("active");
+  menu_right_btn_open.style.display = "flex";
+})
+
+// Đóng mở map iframe
+map_btn.addEventListener("click", () => {
+  console.log("clicked");
+  map_iframe.classList.toggle("active");
+});
+map_close.addEventListener("click", () => {
+  map_iframe.classList.remove("active");
+});
+
+// Đóng mở info iframe
+info_btn.addEventListener("click", () => {
+  info_iframe.classList.toggle("active");
+});
+info_close.addEventListener("click", () => {
+  info_iframe.classList.remove("active");
+});
+
+// Đóng mở video player
+video_1.addEventListener("click", () => {
+  video_player.classList.add("active");
+  main_video.src = `${link_video1}?autoplay=1`;
+  main_audio.pause();
+});
+video_2.addEventListener("click", () => {
+  video_player.classList.add("active");
+  main_video.src = `${link_video2}?autoplay=1`;
+  main_audio.pause();
+});
+video_player_background.addEventListener("click", () => {
+  video_player.classList.remove("active");
+  main_video.src = "";
+  main_audio.play();
+});
+
+// Bật tắt âm thanh
+sound.addEventListener("click", () => {
+  if (main_audio.paused) {
+    main_audio.play();
+    sound_desc.innerText = "Tắt nhạc nền";
+  } else {
+    main_audio.pause();
+    sound_desc.innerText = "Bật nhạc nền";
+  }
+})
+
+let isFullscreen = false;
+function openFullscreen() {
+  if (body.requestFullscreen) {
+    body.requestFullscreen();
+  } else if (body.webkitRequestFullscreen) { /* Safari */
+    body.webkitRequestFullscreen();
+  } else if (body.msRequestFullscreen) { /* IE11 */
+    body.msRequestFullscreen();
+  }
+}
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+fullscreen_btn.addEventListener("click", () => {
+  if (!isFullscreen) {
+    openFullscreen();
+    isFullscreen = true;
+    fullscreen_desc.innerText = "Thoát chế độ toàn màn hình";
+  } else {
+    closeFullscreen();
+    isFullscreen = false;
+    fullscreen_desc.innerText = "Chế độ toàn màn hình";
+  }
+});
+
---
+++ js/nav.js
@@ -0,0 +1,90 @@
+const nav_bar__main = document.querySelector('.nav-bar__main');
+const nav_bar__toggle = document.querySelector('.nav-bar__toggle');
+const nav_bar = document.querySelector('.nav-bar');
+
+nav_bar__toggle.addEventListener('click', () => {
+  nav_bar.classList.toggle("active");
+  nav_bar__toggle.classList.toggle("active");
+  if (nav_bar.classList.contains("active")) {
+    nav_bar__toggle.innerHTML = '<i class="fad fa-angle-double-left"></i>';
+  }
+  else {
+    nav_bar__toggle.innerHTML = '<i class="fas fa-angle-double-right"></i>';
+  }
+});
+
+
+const navDtb = [
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item1",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+  {
+    imgSrc: "./images/thumb.jpg",
+    text: "Toàn cảnh Mithrin Hotel",
+    class: "item2",
+  },
+];
+
+//* Render
+
+const htmls = navDtb.map(navItem => {
+  return `
+    <li class="nav-bar__main__item ${navItem.class}">
+      <div class="nav-bar__main__item__image">
+        <img src="${navItem.imgSrc}" alt="">
+      </div>
+      <div class="nav-bar__main__item__text">
+        <span>${navItem.text}</span>
+      </div>
+      <span class="nav-bar__main__item__tick">✔</span>
+    </li>
+  `;
+}).join('');
+nav_bar__main.innerHTML = htmls;
+
+
+setTimeout(() => {
+  const navItems = document.querySelectorAll('.nav-bar__main__item');
+  navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+      const isActive = document.querySelector(".nav-bar__main__item.active");
+      if (isActive) {
+        isActive.classList.remove('active');
+      }
+      navItem.classList.add('active');
+    })
+  })
+
+}, 0);
